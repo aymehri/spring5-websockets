6ff772fa699351f7d76d95b97fd64edd058d6c67
Aymen Mehri <aymen.mehri@soat.fr>
2017-06-09 16:18:46 +0200

    add websocket tests without rxjs

--- ng-webscoket/src/app/app.component.ts
+++ ng-webscoket/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'io-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'io';
+
+  exampleSocket = new WebSocket('ws://localhost:8080/reverse');
+
+  constructor() { }
+
+  ngOnInit() {
+    this.exampleSocket.onmessage = function (event) {
+      console.log(event.data);
+    }
+  }
+
+  sendMessage(): void {
+    this.exampleSocket.send('text');
+  }
 }
